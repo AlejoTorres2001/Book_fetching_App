100dfa3362996bb08f3b71f2fadf51c076abb5a3
T0w3r <torres.alejo2001@gmail.com>
2021-08-20 18:54:52 -0300

    added book showing partial

--- src/modules/calc-reading.js
+++ src/modules/calc-reading.js
@@ -1,6 +1,10 @@
-const TIME_PER_PAGE=1.6;
-
-function calcReading(pages){
-    return TIME_PER_PAGE * pages / 60
+module.exports={
+    avgTime:(pages) =>
+    {
+        const TIME_PER_PAGE=1.6;
+        return TIME_PER_PAGE * pages / 60
+    }
+    
 }
-export default calcReading
+
+
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const querystring = require('querystring');
 const getBook = require('./modules/get-book');
 const exphbs = require("express-handlebars");
 const path = require("path");
-const { title } = require('process');
+const avgTime=require("./modules/calc-reading")
 //Sets handlebars configurations (we will go through them later on)
 app.set("views", path.join(__dirname, "views"));
 app.engine(
@@ -28,10 +28,15 @@ app.get('/', async (req, res) => {
     if (Object.entries(req.query).length !== 0){
         const code =req.query.code
         const book = await getBook.getBook(code)
+        console.log(book)
         const title = book[code].title
         const number_of_pages = book[code].number_of_pages
+        const subtitle = book[code].subtitle
+        const publish_date = book[code].publish_date
+        const img = await book[code].cover.large
+        const avg = avgTime.avgTime(number_of_pages) 
         res.render('main', 
-        {layout : 'index',title:title,number_of_pages:number_of_pages});
+        {layout : 'index',title:title,subtitle:subtitle,number_of_pages:number_of_pages,publish_date:publish_date,img:img,avg:avg});
     }
     else{
         res.render('main', {layout : 'index'});
